ede4fbf135e587b56f69d1223d6c2076219353d5
Sharon3007 <josephsaju2020@gmail.com>
2026-03-11 14:35:01 +0530

    Initial OOPBannerApp commit

---
+++ OOPBannerApp.java
@@ -0,0 +1,11 @@
+/**
+ * OOPBannerApp - OOP Banner Display Application
+ * This is a simple Java application that displays the OOPs acronym to the console.
+ * @author 1.0
+ */
+ 
+public class OOPBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS!");
+    }
+}
